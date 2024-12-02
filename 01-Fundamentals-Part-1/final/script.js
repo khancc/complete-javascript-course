969270cf34a697bd0117dc9b481975d756286095
khanc <truongkhanga@gmail.com>
2024-12-02 21:21:43 +0700

    as

--- 01-Fundamentals-Part-1/final/script.js
+++ 01-Fundamentals-Part-1/final/script.js
@@ -17,7 +17,7 @@ console.log(firstName);
 
 // Variable name conventions
 let jonas_matilda = "JM";
-let $function = 27;
+let $function =a 27;
 
 let person = "jonas";
 let PI = 3.1415;
---
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -0,0 +1,5 @@
+let js = "amazing";
+console.log((40 - 20 + 300) / 3000);
+console.log("jonas");
+console.log("khanc");
+console.log("23");
