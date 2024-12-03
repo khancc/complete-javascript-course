5c9ea0326958459b0e73e8befe59c081cb668658
khancc <truongkhanga@gmail.com>
2024-12-03 19:24:10 +0700

    done section 1

--- 01-Fundamentals-Part-1/starter/script.js
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -1,15 +1,29 @@
-let js = "amazing";
-console.log((40 - 20 + 300) / 3000);
-let firstName = "dapoet";
-console.log(firstName);
-let num1 = 23;
-let num2 = 33;
-let num1name = "khancdapoetsiuuu";
-let khanc$ = "dollar test";
-let childen; // undefined
-let PI = 3.1415;
+// let jsisfun = true;
+
+// console.log(jsisfun);
+// console.log(typeof jsisfun);
+// jsisfun = "ko vui dau";
+// console.log(typeof jsisfun);
+// let year;
+
+// console.log(year);
+// console.log(typeof year);
+// year = 1996;
+// console.log(typeof year);
+// console.log(typeof null);
 //
-console.log(num1 + num2);
-console.log(khanc$);
-console.log(num1name);
-console.log(PI);
+let khancdapoet = "ten tao";
+console.log(khancdapoet);
+khancdapoet = "23";
+console.log(khancdapoet);
+//
+khanc = "1111";
+console.log(khanc);
+//
+const birthYear = 1991;
+console.log(birthYear);
+const job1 = khanc;
+//
+var job = "programmer";
+job = "bac si";
+console.log(job);
