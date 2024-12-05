ebcbf893cdeb79953a33d9a6f9a5f9592c486a0d
khanc <truongkhanga@gmail.com>
2024-12-05 17:55:03 +0700

    12

--- 01-Fundamentals-Part-1/starter/script.js
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -1,29 +1,58 @@
-// let jsisfun = true;
+// // let jsisfun = true;
 
-// console.log(jsisfun);
-// console.log(typeof jsisfun);
-// jsisfun = "ko vui dau";
-// console.log(typeof jsisfun);
-// let year;
+// // console.log(jsisfun);
+// // console.log(typeof jsisfun);
+// // jsisfun = "ko vui dau";
+// // console.log(typeof jsisfun);
+// // let year;
 
-// console.log(year);
-// console.log(typeof year);
-// year = 1996;
-// console.log(typeof year);
-// console.log(typeof null);
+// // console.log(year);
+// // console.log(typeof year);
+// // year = 1996;
+// // console.log(typeof year);
+// // console.log(typeof null);
+// //
+// let khancdapoet = "ten tao";
+// console.log(khancdapoet);
+// khancdapoet = "23";
+// console.log(khancdapoet);
+// //
+// khanc = "1111";
+// console.log(khanc);
+// //
+// const birthYear = 1991;
+// console.log(birthYear);
+// const job1 = khanc;
+// //
+// var job = "programmer";
+// job = "bac si";
+// console.log(job);
+alert("this is perfect");
+const now = 2024;
+const khancage = now - 2004;
+const sarahage = now - 2008;
+console.log(khancage, sarahage);
+console.log("khanc gioi vc");
+console.log(
+  khancage * 200,
+  sarahage * sarahage * sarahage * sarahage,
+  sarahage ** 4,
+  3 ** 3
+);
+const firstName = "khanc";
+const secondName = "nabe";
+console.log(firstName + " " + secondName);
+let x = 10 + 5; //15
+x -= 10; // x = x - 10 = 15 - 10 = 5
+x /= 5; // 5 / 5 =1
+x++; //1 + 1 = 2
+x--;
+x--;
+console.log(x);
 //
-let khancdapoet = "ten tao";
-console.log(khancdapoet);
-khancdapoet = "23";
-console.log(khancdapoet);
-//
-khanc = "1111";
-console.log(khanc);
-//
-const birthYear = 1991;
-console.log(birthYear);
-const job1 = khanc;
-//
-var job = "programmer";
-job = "bac si";
-console.log(job);
+console.log(khancage > sarahage);
+console.log(sarahage >= 18);
+const isSarahFullAge = sarahage >= 18;
+console.log(now - 2010 > now - 2008);
+console.log("khancdapoet");
+console.log("are you okay mate");
