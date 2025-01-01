bd2c36169251eb5ad22b9b82f29f81f758db6e42
khancc <truongkhanga@gmail.com>
2025-01-01 21:03:04 +0700

    Update script.js

--- 01-Fundamentals-Part-1/starter/script.js
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -3,15 +3,16 @@ const tieuchuan = `18`;
 if (age == tieuchuan) console.log("You just became an adult!");
 if (age == tieuchuan) console.log("tuyet cmn voi");
 if (age == tieuchuan) console.log("tuyet voi lamm BTS");
-
+//
 const momAge = Number(prompt("whats your mom's name"));
 console.log(momAge);
 console.log(typeof momAge);
-
+//
 if (momAge === 23) {
-  console.log("I love BTS");
+  console.log(`I love BTS (you input "${momAge}")`);
 } else if (momAge === 11) {
-  console.log("ur a fkin pervert");
+  console.log(`ur a fkin pervert (you input "${momAge}")`);
 } else {
   console.log(`its not 23 or 11 (you input "${momAge}") `);
 }
+if (momAge !== 23) console.log("Why not 23?");
