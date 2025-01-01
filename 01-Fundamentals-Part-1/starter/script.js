1057e1b1e0b6ab5acbf59e04d0575c00ae9b2908
khancc <truongkhanga@gmail.com>
2025-01-01 20:31:24 +0700

    Update script.js

--- 01-Fundamentals-Part-1/starter/script.js
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -1,52 +1,4 @@
-// const firstname = "khancdapoet";
-// const job = "student";
-// const birthyear = 2004;
-// const now = 2024;
-// const khanc =
-//   "i'm " + firstname + ", a " + (now - birthyear) + " years old " + job + "!";
-// console.log(khanc);
-// const newkhanc = `I'm ${firstname}, a ${now - birthyear} years old ${job} ! `;
-// console.log(newkhanc);
-// console.log(`${199 - 29}`);
-// console.log(`just a normal comment`);
-// console.log(
-//   `thuc su
-// slsad
-// sdssds`
-// );
-// const age = 18;
-// const isOldEnough = age >= 19;
-
-// if (age >= 19) {
-//   console.log(`Sarah can drive`);
-// } else {
-//   const yearsleft = 19 - age;
-//   if (yearsleft == 1) {
-//     console.log(`Sarah has to wait ${yearsleft} year left`);
-//   } else {
-//     console.log(`Sarah has to wait ${yearsleft} years left`);
-//   }
-// }
-// const inputYear = `1991`;
-// console.log(Number(inputYear), inputYear);
-// console.log(Number(inputYear) + 18);
-
-// console.log(typeof NaN);
-// console.log(String(`truongkhang`), 23);
-// console.log(`im 23 years old ${Number(inputYear)}`);
-// const money = 123;
-// if (money) {
-//   console.log(`dont spend it all`);
-// } else {
-//   console.log(`get a fkin job pls`);
-// }
-
-// let height = 0;
-// if (height) {
-//   console.log(`hey manh nhat tg(${height})cm`);
-// } else {
-//   console.log(`height is undefined`);
-const age = 18;
+const age = 16;
 const tieuchuan = `18`;
 if (age == tieuchuan) {
   console.log(`may du tuoi roi`);
@@ -54,5 +6,15 @@ if (age == tieuchuan) {
 else if (age > 18) {
   console.log(`may qua tuoi roi`);
 } else if (age < 18) {
-  console.log(`may chua du tuoi dau`);
+  console.log(`may chua du tuoi dau `);
 }
+console.log(typeof age);
+if (age === tieuchuan) {
+  SVGDefsElement.log(`may du tuoi roi`);
+}
+console.log(`tieuchuan: ${tieuchuan}`);
+alert(`Hello World`);
+
+console;
+
+sd;
