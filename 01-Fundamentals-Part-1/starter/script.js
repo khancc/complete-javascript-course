b97445bfaf40bd9a5625a7bdf40c1262e9ded778
khancc <truongkhanga@gmail.com>
2025-01-02 10:35:10 +0700

    Update script.js

--- 01-Fundamentals-Part-1/starter/script.js
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -1,17 +1,27 @@
-// console.log(hasDriversLicense && hasGoodVision); // true
-// console.log(hasDriversLicense || hasGoodVision); // true
-// console.log(!hasGoodVision); // false
-const scoreDolphins = (96 + 108 + 89) / 3;
-const scoreKoalas = (88 + 91 + 110) / 3;
+const day = `monday`;
+switch (day) {
+  case `monday`:
+    console.log(`have maths class`);
+    console.log(`go to coding meet up`);
+    break;
+  case `tuesday`:
+    console.log(`have science class`);
+    break;
+  case `wednesday`:
+  case `thursday`:
+    console.log(`ur fkin free`);
+    break;
+  default:
+    console.log(`not a valid day`);
+}
 
-if (scoreDolphins > scoreKoalas && scoreDolphins >= 100) {
-  console.log(`Dolphins team win with the result of ${scoreDolphins}`);
-} else if (scoreKoalas > scoreDolphins && scoreKoalas >= 100) {
-  console.log(`Koalas team won with the result of ${scoreKoalas}`);
-} else if (
-  scoreKoalas === scoreDolphins &&
-  scoreKoalas >= 100 &&
-  scoreDolphins >= 100
-) {
-  console.log(`no team won the trophy`);
+if (day === `monday`) {
+  console.log(`have maths class`);
+  console.log(`go to coding meet up`);
+} else if (day === `tuesday`) {
+  console.log(`have science class`);
+} else if (day === `thursday` || day === `wednesday`) {
+  console.log(`ur fkin free`);
+} else {
+  console.log(`not a valid day`);
 }
